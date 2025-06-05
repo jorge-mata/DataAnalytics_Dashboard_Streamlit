1acf76d01a16e8c826bad415fa455787eb415f11
jorge-mata <a00829704@itesm.mx>
2025-06-05 08:07:56 -0600

    Multiple pages

---
+++ pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Acerca de")
+st.write("This is the first additional page.")
---
+++ pages/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("User Guide")
+st.write("This is the second additional page.")
