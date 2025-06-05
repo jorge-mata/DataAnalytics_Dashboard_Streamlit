c7bb3e6f229d00d962a30e6a842df73a29f37355
jorge-mata <a00829704@itesm.mx>
2025-06-05 08:17:06 -0600

    Acerca de basico

---
+++ pages/Acerca de.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Acerca de")
+
+st.header("Introducción / Planteamiento del Problema")
+st.write("""
+Durante estos meses, tuvimos la oportunidad de colaborar con Ximple, una fintech que enfrenta diversos retos, especialmente en la gestión de riesgos. Uno de los principales desafíos es otorgar préstamos a emprendedores y personas que trabajan mediante ventas por catálogo, quienes requieren financiamiento para cumplir con sus ventas.
+
+En este documento, presentamos una narrativa basada en la información recopilada a lo largo del curso, así como la propuesta de modelos de machine learning y dashboard que desarrollamos para abordar y precisar nuestro problema: ¿quiénes son los clientes potenciales que podrían no pagar un préstamo, representando así un riesgo para la empresa?
+""")
+
+st.header("Audiencia Objetivo")
+st.write("""
+Este proyecto está dirigido principalmente al jefe de gestión de riesgos de Ximple, Xavier, quien ha sido nuestro principal contacto y usuario designado del dashboard. Durante este periodo, Xavier nos ha guiado en cada paso para mejorar nuestros modelos de machine learning y los entregables del dashboard, asegurando que nuestra propuesta sea útil y bien estructurada para sus necesidades.
+""")
+
+st.header("Storytelling")
+st.write("""
+A continuación, se muestran los pasos y evidencias del trabajo realizado junto a Xavier. Esta página web ofrece una explicación breve de todos los entregables previos, comenzando por quién es Ximple, el planteamiento del problema, los hallazgos en la base de datos proporcionada, los modelos de machine learning desarrollados, la creación del dashboard y, finalmente, las recomendaciones finales derivadas del proyecto.
+
+**Home / Acerca de**
+- En esta sección, es fundamental mostrar el conocimiento del equipo sobre la empresa (¿quién es Ximple? ¿cuáles son sus retos?), proporcionando un contexto que permita entender el trabajo realizado y demostrar una clara comprensión del negocio.
+- Posteriormente, se describe la situación actual de Ximple, identificando los factores cruciales (internos y externos) y presentando una visión general del análisis FODA.
+- El núcleo del proyecto es el planteamiento del problema: cómo puede la empresa gestionar el riesgo que representan algunos clientes debido al incumplimiento en el pago de préstamos.
+- Finalmente, se introduce la solución propuesta, ofreciendo solo un adelanto, ya que las siguientes páginas profundizarán en cada parte de la solución.
+""")
