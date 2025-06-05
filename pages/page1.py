1845ec65bf6e25b093c6c48432df22bc937e25ea
jorge-mata <a00829704@itesm.mx>
2025-06-05 08:28:58 -0600

    Page naming

--- pages/page1.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Acerca de")
-st.write("This is the first additional page.")
--- pages/page2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("User Guide")
-st.write("This is the second additional page.")
